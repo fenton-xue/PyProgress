7b27dce1b0297c6ed954e514f5f7aad8c061fd10
Fenton <haiyungg1016@gmail.com>
2025-08-08 11:48:12 +0800

    2025/08/08 初始化项目

---
+++ FluentPython/示例1-1.py
@@ -0,0 +1,18 @@
+import collections
+
+Card = collections.namedtuple('Card', ['rank', 'suit'])
+
+class FrenchDeck:
+    ranks = [str(n) for n in range(2, 11)] + list('JQKA')
+    suits = '黑桃 方块 梅花 红心'.split()
+
+    def __init__(self):
+        self._cards = [Card(rank, suit) for suit in self.suits
+                                        for rank in self.ranks]
+
+    def __len__(self):
+        return len(self._cards)
+
+if __name__ == '__main__':
+    print(FrenchDeck.ranks)
+    print(FrenchDeck.suits)
